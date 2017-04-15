0f0b998219e17eb31d404d9fed9ba36bcab7cdbe
Alex Lima <alexlima@MacBook-Pro-de-Alex.local>
2017-04-15 14:05:11 -0300

    Changed the DB URL to use EC2

--- config/database.js
+++ config/database.js
@@ -1,4 +1,3 @@
 module.exports = {
-    remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
-    localUrl: 'mongodb://localhost/meanstacktutorials'
+    localUrl: 'mongodb://user:password@ec2-54-233-125-108.sa-east-1.compute.amazonaws.com:27017/placemark'
 };
